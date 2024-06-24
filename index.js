ba230a6acfea5ffd6043c901655bbeca1f52f5f4
JunW62 <sivia1990@gmail.com>
2024-06-24 14:28:37 -0500

    create cards

--- index.js
+++ index.js
@@ -9,12 +9,34 @@ const fetchPokemons = async () => {
 
 const getPokemon = async (i) => {
   try {
-    const url = `https://pokeapi.co/api/v2/growth-rate/${i}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const response = await fetch(url);
     const pokemon = await response.json();
-    console.log(pokemon);
+    // console.log(pokemon);
+    createPokemonCard(pokemon);
   } catch (err) {
     console.log(err);
   }
+};
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+  const pokemonInnerHTML = `
+        <div class="img-container">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+            alt=""
+          />
+        </div>
+        <div class="info">
+          <span class="num">${pokemon.id}</span>
+          <h3 class="name">${pokemon.name}</h3>
+          <small class="type"> Type <span>${pokemon.types[0].type.name}</span></small>
+        </div>
+   `;
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  pokeContainer.appendChild(pokemonEl);
+  //   console.log(pokemonInnerHTML);
 };
 fetchPokemons();
