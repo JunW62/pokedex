f7ca06f99e13530501f4472211e08a188fc2f7eb
JunW62 <sivia1990@gmail.com>
2024-06-24 14:44:38 -0500

    add color and cap the firstletter

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const pokeContainer = document.getElementById("poke-container");
 const pokemonCount = 150;
 
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const main_types = Object.keys(colors);
+console.log(main_types);
+
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokemonCount; i++) {
     await getPokemon(i);
@@ -22,17 +42,27 @@ const getPokemon = async (i) => {
 const createPokemonCard = (pokemon) => {
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
+  const nameCapitalized =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+  console.log(color);
+
+  pokemonEl.style.backgroundColor = color;
+
   const pokemonInnerHTML = `
         <div class="img-container">
           <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png"
             alt=""
           />
         </div>
         <div class="info">
-          <span class="num">${pokemon.id}</span>
-          <h3 class="name">${pokemon.name}</h3>
-          <small class="type"> Type <span>${pokemon.types[0].type.name}</span></small>
+          <span class="num">#${pokemon.id}</span>
+          <h3 class="name">${nameCapitalized}</h3>
+          <small class="type"> Type : <span>${pokemon.types[0].type.name}</span></small>
         </div>
    `;
   pokemonEl.innerHTML = pokemonInnerHTML;
